8005a979b2b89b265df2ba6689f69faed1f780ed
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-04 17:31:47 -0300

    Update mysql_init_database.sql

--- mysql_init_database.sql
+++ mysql_init_database.sql
@@ -12,6 +12,6 @@ SET character_set_connection = utf8;
 SET character_set_results = utf8;
 SET collation_connection = utf8_general_ci;
 
-INSERT INTO users (full_name,created_at, address) VALUES("Indiana Jones",NOW(),"Falling Ball Avenue");
+INSERT INTO users (full_name,created_at, address) VALUES("Indiana Jones",NOW(),"Adventures Avenue");
 INSERT INTO users (full_name,created_at, address) VALUES("Jack Sparrow",NOW(),"Master Ship Square");
 INSERT INTO users (full_name,created_at, address) VALUES("John Snow",NOW(),"Kinds Landing Westeros");
