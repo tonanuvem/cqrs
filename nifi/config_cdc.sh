7de9c5521a32687e1ab47d310051fe9ace683b36
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-05 09:23:51 -0300

    Create config_cdc.sh

---
+++ nifi/config_cdc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8073)" != "200" ]]; do echo .; sleep 5; done'
+echo "Debezium CDC is up"
+
+HOST_IP=$(curl checkip.amazonaws.com)
+echo $HOST_IP
+
+curl -s -X POST http://localhost:8073/plugins \
+  -d @config_connectors/postgres.json
+  | python -mjson.tool
