1b692088dce85eb2848a6bb6202c792e14c3cf2a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-04 23:09:12 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-echo "Digite a URL da licença do Lenses que você recebeu no seu email: ex: https://licenses.lenses.io/download/lensesdl?id=00000000-0000-0000-0000-000000000000"
-read EULA
+#echo "Digite a URL da licença do Lenses que você recebeu no seu email: ex: https://licenses.lenses.io/download/lensesdl?id=00000000-0000-0000-0000-000000000000"
+#read EULA
+
+EULA=$(cat lensesid.txt)
 
 sed -i 's|LICENSE|'$EULA'|' docker-compose.yml
 
-IP=$(curl checkip.amazonaws.com) 
+#IP=$(curl checkip.amazonaws.com) 
 
-sed -i 's|PUBLIC_IP|'$IP'|' docker-compose.yml
+#sed -i 's|PUBLIC_IP|'$IP'|' docker-compose.yml
 
 docker-compose up -d
